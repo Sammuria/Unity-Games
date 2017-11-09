49ff9a2b984416a4d1d48164dcec9e1a94d8c102
Sowmiya Selva <30924934+Sammuria@users.noreply.github.com>
2017-11-09 19:49:32 +0530

    Unity Apt
    The apartment I made

---
+++ Assets/RockVR/Video/Demo/Scripts/VideoCaptureUI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+namespace RockVR.Video.Demo
+{
+    public class VideoCaptureUI : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.runInBackground = true;
+        }
+
+        private void OnGUI()
+        {
+            if (VideoCaptureCtrl.instance.status == VideoCaptureCtrl.StatusType.NOT_START)
+            {
+                if (GUI.Button(new Rect(10, Screen.height - 60, 150, 50), "Start Capture"))
+                {
+                    VideoCaptureCtrl.instance.StartCapture();
+                }
+            }
+            else if (VideoCaptureCtrl.instance.status == VideoCaptureCtrl.StatusType.STARTED)
+            {
+                if (GUI.Button(new Rect(10, Screen.height - 60, 150, 50), "Stop Capture"))
+                {
+                    VideoCaptureCtrl.instance.StopCapture();
+                }
+                if (GUI.Button(new Rect(180, Screen.height - 60, 150, 50), "Pause / Continue"))
+                {
+                    VideoCaptureCtrl.instance.PauseCapture();
+                }
+            }
+            else if (VideoCaptureCtrl.instance.status == VideoCaptureCtrl.StatusType.STOPPED)
+            {
+                if (GUI.Button(new Rect(10, Screen.height - 60, 150, 50), "Processing"))
+                {
+                    // Waiting processing end.
+                }
+            }
+            else if (VideoCaptureCtrl.instance.status == VideoCaptureCtrl.StatusType.FINISH)
+            {
+                if (GUI.Button(new Rect(10, Screen.height - 60, 150, 50), "View Video"))
+                {
+                    // Open video save directory.
+                    Process.Start(PathConfig.saveFolder);
+                }
+            }
+        }
+    }
+}
